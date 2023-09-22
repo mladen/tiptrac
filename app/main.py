d4635faf6f5648504efc07cdcf1b40b17d79f041
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2023-09-22 20:08:25 +0200

    Replaces the TASKS list with the PROJECTS list (that contains the tasks for that project)

--- app/main.py
+++ app/main.py
@@ -4,27 +4,53 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-TASKS = [
+PROJECTS = [
     {
         "id": 1,
-        "title": "Buy groceries",
-        "description": "Milk, Cheese, Pizza, Fruit",
-        "done": False,
-    },
-    {
-        "id": 2,
         "title": "Create a Time and Project Tracker",
         "description": "Need to create a time and project tracker for myself",
         "done": False,
-    },
+        "tasks": [
+            {
+                "id": 1,
+                "title": "Setup the project",
+                "description": "Setup the project with FastAPI",
+                "done": False,
+            },
+            {
+                "id": 2,
+                "title": "Create the project model",
+                "description": "Create the project model with Pydantic",
+                "done": False,
+            },
+            {
+                "id": 3,
+                "title": "Create the project endpoints",
+                "description": "Create the project endpoints with FastAPI",
+                "done": False,
+            },
+            {
+                "id": 4,
+                "title": "Create the project database",
+                "description": "Create the project database with SQLAlchemy",
+                "done": False,
+            },
+            {
+                "id": 5,
+                "title": "Create the project database",
+                "description": "Create the project database with SQLAlchemy",
+                "done": False,
+            },
+        ],
+    }
 ]
 
 
 @app.get("/")
-def read_all_tasks():
-    return {"Tasks": TASKS}
+def read_all_projects():
+    return {"Projects": PROJECTS}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/projects/{project_id}")
+def read_project(project_id: int, q: Union[str, None] = None):
+    return {"project_id": project_id, "q": q}
