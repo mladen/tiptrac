3cfd70454b36934b5e3aaea2e3e34827cd89c39c
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2023-09-25 13:13:52 +0200

    Creates models for Task and Project; creates endpoints for creating a Task and a Project

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -46,6 +48,22 @@ PROJECTS = [
 ]
 
 
+class Task(BaseModel):
+    id: int
+    title: str
+    description: str
+    done: bool
+
+
+class Project(BaseModel):
+    id: int
+    title: str
+    description: str
+    done: bool
+    tasks: List[Task]
+
+
+# GET
 # Retrieve all projects
 @app.get("/")
 def get_all_projects():
@@ -68,3 +86,19 @@ def get_project_tasks(project_id: int):
 @app.get("/projects/{project_id}/tasks/{task_id}")
 def get_project_task(project_id: int, task_id: int):
     return PROJECTS[project_id - 1]["tasks"][task_id - 1]
+
+
+# POST
+# Post a project
+@app.post("/projects")
+def create_project(project: dict):
+    # def create_project(project: Project):
+    PROJECTS.append(project)
+    return {"Project": project}
+
+
+# Post a task to a project
+@app.post("/projects/{project_id}/tasks")
+def create_project_task(project_id: int, task: dict):
+    PROJECTS[project_id - 1]["tasks"].append(task)
+    return {"Task": task}
