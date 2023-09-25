9a84c535ebf7327135117fa71b3c502871bc6ac5
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2023-09-25 13:14:58 +0200

    Makes all functions asynchronous

--- app/main.py
+++ app/main.py
@@ -66,39 +66,39 @@ class Project(BaseModel):
 # GET
 # Retrieve all projects
 @app.get("/")
-def get_all_projects():
+async def get_all_projects():
     return {"Projects": PROJECTS}
 
 
 # Retrieve a project
 @app.get("/projects/{project_id}")
-def get_project(project_id: int):
+async def get_project(project_id: int):
     return PROJECTS[project_id - 1]
 
 
 # Retrieve all tasks from a project
 @app.get("/projects/{project_id}/tasks")
-def get_project_tasks(project_id: int):
+async def get_project_tasks(project_id: int):
     return PROJECTS[project_id - 1]["tasks"]
 
 
 # Retrieve a task from a project
 @app.get("/projects/{project_id}/tasks/{task_id}")
-def get_project_task(project_id: int, task_id: int):
+async def get_project_task(project_id: int, task_id: int):
     return PROJECTS[project_id - 1]["tasks"][task_id - 1]
 
 
 # POST
 # Post a project
 @app.post("/projects")
-def create_project(project: dict):
-    # def create_project(project: Project):
+async def create_project(project: dict):
+    # async def create_project(project: Project):
     PROJECTS.append(project)
     return {"Project": project}
 
 
 # Post a task to a project
 @app.post("/projects/{project_id}/tasks")
-def create_project_task(project_id: int, task: dict):
+async def create_project_task(project_id: int, task: dict):
     PROJECTS[project_id - 1]["tasks"].append(task)
     return {"Task": task}
