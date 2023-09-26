9d41966232edb34a90a48b85045dfafbebeb9db9
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2023-09-26 14:18:13 +0200

    Organizes API routes with tags for Swagger UI

--- app/main.py
+++ app/main.py
@@ -1,48 +1,58 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, HTTPException
 from .models import Project, Task  # importing models
 from .test_data import PROJECTS  # importing test data
 
+tags_metadata = [
+    {
+        "name": "projects",
+        "description": "Operations related to projects.",
+    },
+    {
+        "name": "tasks",
+        "description": "Operations related to tasks within projects.",
+    },
+]
 
-app = FastAPI()
+app = FastAPI(openapi_tags=tags_metadata)
 
 
 # GET
 # Retrieve all projects
-@app.get("/")
+@app.get("/", tags=["projects"])
 async def get_all_projects():
     return {"Projects": PROJECTS}
 
 
 # Retrieve a project
-@app.get("/projects/{project_id}")
+@app.get("/projects/{project_id}", tags=["projects"])
 async def get_project(project_id: int):
     return PROJECTS[project_id - 1]
 
 
 # Retrieve all tasks from a project
-@app.get("/projects/{project_id}/tasks")
+@app.get("/projects/{project_id}/tasks", tags=["tasks"])
 async def get_project_tasks(project_id: int):
     return PROJECTS[project_id - 1]["tasks"]
 
 
 # Retrieve a task from a project
-@app.get("/projects/{project_id}/tasks/{task_id}")
+@app.get("/projects/{project_id}/tasks/{task_id}", tags=["tasks"])
 async def get_project_task(project_id: int, task_id: int):
     return PROJECTS[project_id - 1]["tasks"][task_id - 1]
 
 
 # POST
 # Post a project
-@app.post("/projects")
+@app.post("/projects", tags=["projects"])
 async def create_project(project: Project):
     PROJECTS.append(project)
     return {"Project": project}
 
 
 # Post a task to a project
-@app.post("/projects/{project_id}/tasks")
+@app.post("/projects/{project_id}/tasks", tags=["tasks"])
 async def create_project_task(project_id: int, task: dict):
     PROJECTS[project_id - 1]["tasks"].append(task)
     return {"Task": task}
