002df983b7e50175b9ecb37582c6ed6f98a2111f
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2023-09-26 18:56:44 +0200

    Removes the min lenght of the description (so that it wouldn't be mandatory)

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Task(BaseModel):
     id: UUID
     title: str = Field(..., min_length=3, max_length=50)
     description: Optional[str] = Field(
-        None, title="Description of the task", min_length=3, max_length=50
+        None, title="Description of the task", max_length=50
     )
     time_spent: int = Field(0, description="Time spent on this task in minutes")
     assigned_to: Optional[str] = Field(
@@ -23,7 +23,7 @@ class Project(BaseModel):
     id: UUID
     title: str = Field(..., min_length=3, max_length=50)
     description: Optional[str] = Field(
-        None, title="Description of the project", min_length=3, max_length=50
+        None, title="Description of the project", max_length=50
     )
     assigned_to: Optional[str] = Field(
         None,
