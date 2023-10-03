e9dd18d37633d881d90890ba8ab3a1b8f0c25d8c
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2023-10-03 14:30:45 +0200

    Uses "UUID" instead of "str" in schemas.py; sets the default values for "status" props

--- app/schemas.py
+++ app/schemas.py
@@ -2,7 +2,7 @@
 
 from pydantic import BaseModel, Field
 from typing import List, Optional
-from uuid import UUID, uuid4
+from uuid import UUID
 
 from .enums import Role, Status
 
@@ -24,14 +24,10 @@ class Task(BaseModel):
         None, title="Description of the task", max_length=50
     )
     time_spent: int = Field(0, description="Time spent on this task in minutes")
-    assigned_to: Optional[str] = Field(
-        None,
-        description="The person responsible for completing this task",
-        min_length=3,
-        max_length=50,
+    assigned_to: Optional[UUID] = Field(
+        None, description="The user ID responsible for completing this task"
     )
-    done: bool = False
-    status: Status = Field(..., description="Status of the task")
+    status: Status = Field(..., description="Status of the task", default=Status.TODO)
 
 
 class Project(BaseModel):
@@ -40,12 +36,10 @@ class Project(BaseModel):
     description: Optional[str] = Field(
         None, title="Description of the project", max_length=50
     )
-    assigned_to: Optional[str] = Field(
-        None,
-        description="The person responsible for this project",
-        min_length=3,
-        max_length=50,
+    assigned_to: Optional[UUID] = Field(
+        None, description="The user ID responsible for this project"
     )
-    done: bool = False
-    status: Status = Field(..., description="Status of the project")
+    status: Status = Field(
+        ..., description="Status of the project", default=Status.TODO
+    )
     tasks: List[Task] = Field([], description="List of tasks related to this project")
