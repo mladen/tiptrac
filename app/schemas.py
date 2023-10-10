1e5666651a09ed04b4773e1b68e0f7279e5b00b7
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2023-10-10 15:54:56 +0200

    Updates schemas.py by connecting Task with Project; also makes several things Optional

--- app/schemas.py
+++ app/schemas.py
@@ -12,9 +12,9 @@ class User(BaseModel):
     name: str = Field(..., min_length=3, max_length=50)
     email: str = Field(..., min_length=3, max_length=50)
     role: Role = Field(..., description="Role of the user")
-    projects: List[UUID] = Field(
-        [], description="List of projects related to this user"
-    )
+    projects: Optional[List[UUID]] = Field(
+        default=[], description="List of projects related to this user"
+    )  # Optional since not every user might be assigned to a project immediately
 
 
 class Task(BaseModel):
@@ -30,6 +30,7 @@ class Task(BaseModel):
     assigned_to: Optional[UUID] = Field(
         None, description="The user ID responsible for completing this task"
     )
+    project_id: UUID  # The project ID to which this task belongs
     status: Status = Field(default=Status.TODO, description="Status of the task")
 
 
@@ -49,4 +50,6 @@ class Project(BaseModel):
         None, description="The user ID responsible for this project"
     )
     status: Status = Field(default=Status.TODO, description="Status of the project")
-    tasks: List[Task] = Field([], description="List of tasks related to this project")
+    tasks: Optional[List[Task]] = Field(
+        default=[], description="List of tasks related to this project"
+    )  # Optional since not every project will have tasks immediately
