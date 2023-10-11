328242d58e73526c0ae1d4edbc76ad4a8e0d4028
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2023-10-11 15:30:20 +0200

    Specifies lenght of several fields

--- app/models.py
+++ app/models.py
@@ -14,10 +14,10 @@ class User(Base):
     id = Column(
         String(36), primary_key=True, default=str(uuid4()), unique=True, index=True
     )
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
+    username = Column(String(50), unique=True, index=True)
+    email = Column(String(70), unique=True, index=True)
     role = Column(Enum(Role), index=True)
-    hashed_password = Column(String)
+    hashed_password = Column(String(255))
     is_active = Column(Boolean, default=True)
 
 
@@ -27,8 +27,8 @@ class Project(Base):
     id = Column(
         String(36), primary_key=True, default=str(uuid4()), unique=True, index=True
     )
-    title = Column(String, unique=True, index=True)
-    description = Column(String)
+    title = Column(String(255), unique=True, index=True)
+    description = Column(String(255))
     assigned_to = Column(
         String(36), ForeignKey("users.id")
     )  # Because a User's id is a UUID
@@ -49,8 +49,8 @@ class Task(Base):
     id = Column(
         String(36), primary_key=True, default=str(uuid4()), unique=True, index=True
     )
-    title = Column(String, unique=True, index=True)
-    description = Column(String)
+    title = Column(String(255), unique=True, index=True)
+    description = Column(String(255))
     time_estimation = Column(
         Integer, default=0
     )  # Estimated time for the task in minutes
