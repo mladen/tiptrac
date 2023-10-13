1abfb86addcc0883258d8b4c8a7bbf088e5f85bc
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2023-10-14 00:19:28 +0200

    Makes many fields (in Pydantic schemes) optional

--- app/schemas.py
+++ app/schemas.py
@@ -34,29 +34,37 @@ class Task(BaseModel):
     id: UUID
     title: str = Field(..., min_length=3, max_length=50)
     description: Optional[str] = Field(
-        None, title="Description of the task", max_length=50
+        None, title="Description of the task", max_length=255
     )
-    time_estimation: int = Field(
+    time_estimation: Optional[int] = Field(
         0, description="Estimated time for the task in minutes"
     )
-    time_spent: int = Field(0, description="Time already spent on the task in minutes")
+    time_spent: Optional[int] = Field(
+        0, description="Time already spent on the task in minutes"
+    )
     assigned_to: Optional[UUID] = Field(
         None, description="The user ID responsible for completing this task"
     )
-    project_id: UUID  # The project ID to which this task belongs
+    project_id: Optional[UUID] = Field(
+        None, description="The project ID this task is related to"
+    )  # The project ID to which this task belongs
     status: Status = Field(default=Status.TODO, description="Status of the task")
+
+    user: Optional[User] = Field(
+        None, description="The user responsible for completing this task"
+    )
 
 
 class Project(BaseModel):
     id: UUID
     title: str = Field(..., min_length=3, max_length=50)
     description: Optional[str] = Field(
-        None, title="Description of the project", max_length=50
+        None, title="Description of the project", max_length=255
     )
-    time_estimation: int = Field(
+    time_estimation: Optional[int] = Field(
         0, description="Estimated time for the project in minutes"
     )
-    time_spent: int = Field(
+    time_spent: Optional[int] = Field(
         0, description="Time already spent on the project in minutes"
     )
     assigned_to: Optional[UUID] = Field(
@@ -66,3 +74,62 @@ class Project(BaseModel):
     tasks: Optional[List[Task]] = Field(
         default=[], description="List of tasks related to this project"
     )  # Optional since not every project will have tasks immediately
+
+
+# class TaskResponse(BaseModel):
+#     id: UUID
+#     title: str
+#     description: Optional[str] = None
+#     time_estimation: Optional[int] = 0
+#     time_spent: Optional[int] = 0
+#     assigned_to: Optional[UUID] = None
+#     project_id: Optional[UUID] = None
+#     status: Status
+
+
+class TaskResponse(BaseModel):
+    id: UUID
+    title: str = Field(..., min_length=3, max_length=50)
+    description: Optional[str] = Field(
+        None, title="Description of the task", max_length=255
+    )
+    time_estimation: Optional[int] = Field(
+        0, description="Estimated time for the task in minutes"
+    )
+    time_spent: Optional[int] = Field(
+        0, description="Time already spent on the task in minutes"
+    )
+    assigned_to: Optional[UUID] = Field(
+        None, description="The user ID responsible for completing this task"
+    )
+    project_id: Optional[UUID] = Field(
+        None, description="The project ID this task is related to"
+    )  # The project ID to which this task belongs
+    status: Status = Field(default=Status.TODO, description="Status of the task")
+
+    class Config:
+        from_attributes = True
+
+
+class ProjectResponse(BaseModel):
+    id: UUID
+    title: str = Field(..., min_length=3, max_length=50)
+    description: Optional[str] = Field(
+        None, title="Description of the project", max_length=255
+    )
+    time_estimation: Optional[int] = Field(
+        0, description="Estimated time for the project in minutes"
+    )
+    time_spent: Optional[int] = Field(
+        0, description="Time already spent on the project in minutes"
+    )
+    assigned_to: Optional[UUID] = Field(
+        None, description="The user ID responsible for this project"
+    )
+    status: Status = Field(default=Status.TODO, description="Status of the project")
+    tasks: Optional[List[Task]] = Field(
+        default=[], description="List of tasks related to this project"
+    )  # Optional since not every project will have tasks immediately
+
+    class Config:
+        from_attributes = True
