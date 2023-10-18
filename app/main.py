c40d1e22b54ab1a21e428759c8072afe908fb0e9
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2023-10-18 15:41:41 +0200

    Fixes the function for retrieving a (single) user

--- app/main.py
+++ app/main.py
@@ -83,7 +83,7 @@ async def get_users(db: Session = Depends(get_db)):
 # Retrieve a user
 @app.get("/users/{user_id}", response_model=schemas.UserResponse, tags=["users"])
 async def get_user(user_id: UUID, db: Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    db_user = db.query(models.User).filter(models.User.id == str(user_id)).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
