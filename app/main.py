30dc0322e907aad64fce013ed11a2a2a0259e756
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2023-10-20 18:22:08 +0200

    Fixes an error that happens while trying to retrieve a list of user's projects

--- app/main.py
+++ app/main.py
@@ -266,7 +266,7 @@ async def delete_project(project_id: UUID, db: Session = Depends(get_db)):
     tags=["projects"],
 )
 async def get_user_projects(user_id: UUID, db: Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    db_user = db.query(models.User).filter(models.User.id == str(user_id)).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user.projects
